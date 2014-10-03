ca98c4f84af1da055ff5fab82a34b1ae93b209eb
x00092816 <x00092816@ittd.ie>
2014-10-03 15:42:55 +0100

    added details

--- Week3/script.sh
+++ Week3/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#: Title : word count 
+#: Title : CPU count 
 #: Date : 03/10/2014
 #: Author : x00092816
 #: Version :1
-#: Description : 
+#: Description : a script to show the cpu count
 #: Options : 
 
 cpucount=$( grep processor /proc/cpuinfo | wc -l )
